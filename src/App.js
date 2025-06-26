cd21fd819a5658101f064228ddba1b652bec13d1
Dnyaneshwari Khatiwada <137375437+diya200d@users.noreply.github.com>
2025-06-26 23:19:20 +0530

    Deploy React app to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,75 +1,56 @@
-
 import './App.css';
-import Header from "./MyComponents/Header"
-import Todos from "./MyComponents/Todos"
-import Footer from "./MyComponents/Footer"
-import AddTodo from "./MyComponents/AddTodo"
-import About from "./MyComponents/About"
+import Header from "./MyComponents/Header";
+import Todos from "./MyComponents/Todos";
+import Footer from "./MyComponents/Footer";
+import AddTodo from "./MyComponents/AddTodo";
+import About from "./MyComponents/About";
 import { useEffect, useState } from "react";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
 function App() {
   let initTodo;
-  // get item returns null
-  if(localStorage.getItem("todos")===null){
-   initTodo=[];
+  if(localStorage.getItem("todos") === null){
+    initTodo = [];
   }
   else{
-    // set init to to
-    initTodo= JSON.parse(localStorage.getItem("todos"));
+    initTodo = JSON.parse(localStorage.getItem("todos"));
   }
- 
- const onDelete =(todo)=>{
-   console.log("I am on delete of todo",todo)
-  //  Deleteing this way in react doesnt works use setTodos
-  //  let index=todo.indexOf(todo);
-  //  todos.splice(index,1);
 
-  //  filter is higher order method for array
-  setTodos(todos.filter((e)=>{
-    return e!==todo;
-  }))
-  localStorage.setItem("todos",JSON.stringify(todos));
-  }
-  const addTodo=(title,desc)=>{
-    console.log("I am adding ths to do ",title,desc)
-    let sno;
-    if(todos.length==0){
-      sno=1;
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const savedTodos = JSON.parse(localStorage.getItem("todos"));
+    if (savedTodos) {
+      setTodos(savedTodos);
     }
-    else{
-      sno=todos[todos.length-1].sno+1;
-    }
-     const myTodo = {
-    sno: sno,
-    title: title,
-    desc: desc
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const onDelete = (todo) => {
+    setTodos(todos.filter((e) => e !== todo));
+    localStorage.setItem("todos", JSON.stringify(todos));
   };
 
-  console.log(myTodo);
-  setTodos([...todos, myTodo]);
-};
+  const addTodo = (title, desc) => {
+    let sno;
+    if (todos.length === 0) {
+      sno = 1;
+    } else {
+      sno = todos[todos.length - 1].sno + 1;
+    }
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    };
+    setTodos([...todos, myTodo]);
+  };
 
-  // set todos is a func which updates todo
-const [todos, setTodos] = useState([]);
-
-// Load todos once when component mounts
-useEffect(() => {
-  const savedTodos = JSON.parse(localStorage.getItem("todos"));
-  if (savedTodos) {
-    setTodos(savedTodos);
-  }
-}, []);
-
-// Save todos to localStorage whenever todos state changes
-useEffect(() => {
-  localStorage.setItem("todos", JSON.stringify(todos));
-}, [todos]);
-
-
-  
-
-return (
+  return (
     <Router basename="/React-My-Todos-List">
       <Header title="My Todos List" searchBar={false} />
 
@@ -84,6 +65,8 @@ return (
           }
         />
         <Route path="/about" element={<About />} />
+        {/* Redirect all other routes to home */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
 
       <Footer />
